8abeb6ad7fe456a72f64ba6596a248af4f17eaa3
Phil Reynolds <phil.d.reynolds@gmail.com>
2021-05-06 23:49:11 -0400

    Lesson 2 - Mock API and replace App.vue

---
+++ src/components/ProductList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <ul>
+      <li v-for="product in products">{{product.title}} - {{product.price}}</li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+import shop from '@/api/shop'
+
+export default {
+  name: 'ProductList',
+  data () {
+    return {
+      products: []
+    }
+  },
+  created () {
+    shop.getProducts(products => {
+      this.products = products
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
